3f1f4706bb20f4b52b113bb46d919a09c01dcb2a
Fedor Kuzminov <riyce@mail.ru>
2025-01-17 00:55:00 +0100

    .

--- docker-restart.sh
+++ docker-restart.sh
@@ -1,3 +1,10 @@
-sudo docker compose -f docker-compose.yaml down
-sudo docker compose -f docker-compose.yaml pull
-sudo docker compose -f docker-compose.yaml up -d
+#!/bin/sh
+
+docker_file="docker-compose.yaml"
+backend_container_name="backend"
+
+sudo docker compose -f ${docker_file} down
+sudo docker compose -f ${docker_file} pull
+sudo docker compose -f ${docker_file} up -d
+sudo docker exec -it ${backend_container_name} bash python manage.py migrate
+sudo docker exec -it ${backend_container_name} bash python manage.py collectstatic --noinput
