74a57263c6b57fea23d99e2aad961ef6d75d13a4
Fedor Kuzminov <riyce@mail.ru>
2025-01-17 01:21:13 +0100

    appleboy/ssh-action@master

--- docker-restart.sh
+++ docker-restart.sh
@@ -6,5 +6,5 @@ backend_container_name="backend"
 sudo docker compose -f ${docker_file} down
 sudo docker compose -f ${docker_file} pull
 sudo docker compose -f ${docker_file} up -d
-sudo docker exec -it ${backend_container_name} bash python3 manage.py migrate
-sudo docker exec -it ${backend_container_name} bash python3 manage.py collectstatic --noinput
+sudo docker exec ${backend_container_name} python manage.py migrate
+sudo docker exec ${backend_container_name} python manage.py collectstatic --noinput
