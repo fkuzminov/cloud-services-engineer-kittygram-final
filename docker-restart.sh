9984ddc8e355938c7b8e85efe6b8b5e69ce889c9
Fedor Kuzminov <riyce@mail.ru>
2025-04-10 16:20:21 +0200

    rm sudo

--- docker-restart.sh
+++ docker-restart.sh
@@ -3,8 +3,11 @@
 docker_file="docker-compose.production.yml"
 backend_container_name="backend"
 
-sudo docker compose -f ${docker_file} down
-sudo docker compose -f ${docker_file} pull
-sudo docker compose -f ${docker_file} up -d
-sudo docker exec ${backend_container_name} python manage.py migrate
-sudo docker exec ${backend_container_name} python manage.py collectstatic --noinput
+# For the first run when we don't have any active containers
+set +e
+docker compose -f ${docker_file} down
+set -e
+docker compose -f ${docker_file} pull
+docker compose -f ${docker_file} up -d
+docker exec ${backend_container_name} python manage.py migrate
+docker exec ${backend_container_name} python manage.py collectstatic --noinput
