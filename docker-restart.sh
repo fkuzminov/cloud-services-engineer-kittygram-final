e2de88e83a64154354e924d57b0164f83d337e55
Fedor Kuzminov <riyce@mail.ru>
2025-04-10 16:30:12 +0200

    it's time to finish

--- docker-restart.sh
+++ docker-restart.sh
@@ -5,9 +5,9 @@ backend_container_name="backend"
 
 # For the first run when we don't have any active containers
 set +e
-docker compose -f ${docker_file} down
+docker-compose --file ${docker_file} down
 set -e
-docker compose -f ${docker_file} pull
-docker compose -f ${docker_file} up -d
+docker-compose --file ${docker_file} pull
+docker-compose --file ${docker_file} up -d
 docker exec ${backend_container_name} python manage.py migrate
 docker exec ${backend_container_name} python manage.py collectstatic --noinput
